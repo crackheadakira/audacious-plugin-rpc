acbe688ee0795fc479d5b52d24269cbf0bc14e3a
crackheadakira <lasn.mine@gmail.com>
2022-10-30 19:36:35 +0100

    Almost working, just messing up on getting album url from nested json

--- audacious-plugin-rpc.cc
+++ audacious-plugin-rpc.cc
@@ -3,6 +3,7 @@
 
 #include "HTTPRequest.hpp"
 #include "json.hpp"
+#include "encode.h"
 
 #include <libaudcore/drct.h>
 #include <libaudcore/i18n.h>
@@ -19,6 +20,7 @@
 #define APPLICATION_ID "1036306255507095572"
 
 static const char *SETTING_EXTRA_TEXT = "extra_text";
+using json = nlohmann::json;
 
 class RPCPlugin : public GeneralPlugin
 {
@@ -76,56 +78,67 @@ void cleanup_discord()
 
 void title_changed()
 {
-
-    if (!aud_drct_get_ready())
+    try
     {
-        return;
-    }
+        const char *albumCoverURL;
 
-    if (aud_drct_get_playing())
-    {
-        bool paused = aud_drct_get_paused();
-        Tuple tuple = aud_drct_get_tuple();
-        String artist = tuple.get_str(Tuple::Artist);
-        std::string title(tuple.get_str(Tuple::Title));
-
-        std::string requestURL("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=29c8a554e57d377f721cf665d14f6b5f&artist=straykids&album=noeasy&format=json");
-        http::Request request{requestURL};
-        const auto response = request.send("GET");
-        std::string responseResult{response.body.begin(), response.body.end()};
-        using json = nlohmann::json;
-        auto responseJson = json::parse(responseResult);
-        std::string albumCoverURLString = responseJson["album"]["image"][3]["#text"];
-        const char *albumCoverURL = albumCoverURLString.c_str();
-
-        presence.largeImageKey = albumCoverURL;
-
-        if (artist)
+        if (!aud_drct_get_ready())
         {
-            fullTitle = (std::string(artist) + " - " + title).substr(0, 127);
+            return;
+        }
+
+        if (aud_drct_get_playing())
+        {
+            bool paused = aud_drct_get_paused();
+            Tuple tuple = aud_drct_get_tuple();
+            std::string artist(tuple.get_str(Tuple::Artist));
+            std::string album(tuple.get_str(Tuple::Album));
+            std::string title(tuple.get_str(Tuple::Title));
+
+            std::string requestURL("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=29c8a554e57d377f721cf665d14f6b5f&artist=" + url_encode(artist) + "&album=" + url_encode(album) + "&format=json");
+
+            http::Request request{requestURL};
+            const auto response = request.send("GET");
+            std::string responseData(response.body.begin(), response.body.end());
+            std::cout << requestURL + "\n";
+
+            json responseJson = json::parse(responseData);
+
+            albumCoverURL = std::string(responseJson["album"]["image"][3]["#text"]).c_str();
+
+            if (artist.length() > 0)
+            {
+                fullTitle = (std::string(artist) + " - " + title).substr(0, 127);
+            }
+            else
+            {
+                fullTitle = title.substr(0, 127);
+            }
+
+            playingStatus = paused ? "Paused" : "Listening";
+
+            presence.details = fullTitle.c_str();
+            presence.smallImageKey = paused ? "pause" : "play";
         }
         else
         {
-            fullTitle = title.substr(0, 127);
+            playingStatus = "Stopped";
+            presence.state = "Stopped";
+            presence.smallImageKey = "stop";
         }
 
-        playingStatus = paused ? "Paused" : "Listening";
+        std::string extraText(aud_get_str("audacious-plugin-rpc", SETTING_EXTRA_TEXT));
+        playingStatus = (playingStatus + " " + extraText).substr(0, 127);
+        // presence.largeImageKey = albumCoverURL;
+        std::cout << std::string(albumCoverURL) + "\n";
 
-        presence.details = fullTitle.c_str();
-        presence.smallImageKey = paused ? "pause" : "play";
+        presence.state = playingStatus.c_str();
+        update_presence();
     }
-    else
+    catch (const std::exception &exc)
     {
-        playingStatus = "Stopped";
-        presence.state = "Stopped";
-        presence.smallImageKey = "stop";
+        presence.largeImageKey = "logo";
     }
-
-    std::string extraText(aud_get_str("audacious-plugin-rpc", SETTING_EXTRA_TEXT));
-    playingStatus = (playingStatus + " " + extraText).substr(0, 127);
-
-    presence.state = playingStatus.c_str();
-    update_presence();
 }
 
 void update_title_presence(void *, void *)
@@ -162,7 +175,7 @@ void RPCPlugin::cleanup()
     cleanup_discord();
 }
 
-const char RPCPlugin::about[] = N_("Discord RPC music status plugin\n\nWritten by: Derzsi Daniel <daniel@tohka.us> \n\n Edited by: crackheadakira, Sayykii, Tibix”");
+const char RPCPlugin::about[] = N_("Discord RPC music status plugin\n\nWritten by: Derzsi Daniel <daniel@tohka.us> \n\n Edited by: crackheadakira, Sayykii, Tibix");
 
 const PreferencesWidget RPCPlugin::widgets[] =
     {
